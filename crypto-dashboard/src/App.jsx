a1dc309bc073be1503b6a2ec91f48120c29f309e
Juanjito G <129878536+gomsago@users.noreply.github.com>
2026-04-18 16:05:17 -0500

    Paso 1, 2 y 3 terminados. Trabajando en el paso 4

--- crypto-dashboard/src/App.jsx
+++ crypto-dashboard/src/App.jsx
@@ -1,30 +1,54 @@
 import Test from "@/components/Test";
 import { useEffect } from "react";
-
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  useEffect(() => {
-    console.log("Hola desde useEffect");
-    fetch("/api/v2/assets")
-    .then((response) =>{
-      console.log("STATUS:", response.status);
-      console.log("OK:", response.ok);
-      return response.json();
-    })
-    .then((data) => {
-      console.log("DATA:", data);
-    }) 
-    .catch((error) => {
-      console.error("ERROR:", error);
-    });
-  
-  }, []);
+  const fetchCoins = async() => {
 
+    const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd");
+    
+    if (!response.ok) {
+      throw new Error("Error en la API");
+    }
+
+    const data = await response.json();
+    console.log("DATA:", data);
+
+    return data;
+  };
+
+  const {data, isLoading, isError, refetch} = useQuery ({
+    queryKey: ["coins"],
+    queryFn: fetchCoins,
+    staleTime: 1000 * 60,
+    retry: false,
+    refetchInterval: 1000 * 30,
+  });
+
+
+  if (isLoading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (isError) {
+    return <p>Error: {error}</p>;
+  }
 
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Crypto Dashboard</h1>
-      <p>Base de estilos funcionando</p>
+    <div>
+    <button onClick={() => refetch()}>
+      Actualizar datos
+    </button>
+    {
+      data?.map((coin) => (
+       
+        <div key={coin.id}>
+          <p>{coin.name}</p>
+          <p>{coin.current_price}</p>
+        </div>
+      ))
+    }
     </div>
   );
 }
