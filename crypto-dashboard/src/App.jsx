2620a3076561a18ab296af27828828438ecdfe57
Juanjito G <129878536+gomsago@users.noreply.github.com>
2026-04-18 13:35:40 -0500

    Base cread Primer fetch

--- crypto-dashboard/src/App.jsx
+++ crypto-dashboard/src/App.jsx
@@ -1,11 +1,31 @@
 import Test from "@/components/Test";
+import { useEffect } from "react";
+
 
 function App() {
+  useEffect(() => {
+    console.log("Hola desde useEffect");
+    fetch("/api/v2/assets")
+    .then((response) =>{
+      console.log("STATUS:", response.status);
+      console.log("OK:", response.ok);
+      return response.json();
+    })
+    .then((data) => {
+      console.log("DATA:", data);
+    }) 
+    .catch((error) => {
+      console.error("ERROR:", error);
+    });
+  
+  }, []);
+
+
   return (
-    <>
+    <div style={{ padding: "20px" }}>
       <h1>Crypto Dashboard</h1>
-      <Test />
-    </>
+      <p>Base de estilos funcionando</p>
+    </div>
   );
 }
 
--- crypto-dashboard/vite.config.js
+++ crypto-dashboard/vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
+
+
+
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    alias: {      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.coincap.io',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
